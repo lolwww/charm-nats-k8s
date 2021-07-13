9f975154e42aa229b8c7f825006a096905b98e8f
lolwww <me@litvinov.co.nz>
2021-07-13 20:47:50 +0300

    working cluster version with conf file generation

--- src/charm.py
+++ src/charm.py
@@ -5,18 +5,14 @@
 # Learn more at: https://juju.is/docs/sdk
 
 import logging
-from configparser import ConfigParser
+from jinja2 import Environment, FileSystemLoader
 
 from ops.charm import CharmBase
-from ops.pebble import ChangeError
 from ops.framework import StoredState
 from ops.main import main
-from ops.model import ActiveStatus, ModelError, MaintenanceStatus, BlockedStatus, WaitingStatus
-from oci_image import OCIImageResource, OCIImageResourceError
-#from serialized_data_interface import NoCompatibleVersions, NoVersionsListed, get_interfaces
+from ops.model import ActiveStatus
 
 from charms.nginx_ingress_integrator.v0.ingress import IngressRequires
-#from ops.pebble import Layer, ConnectionError
 
 logger = logging.getLogger(__name__)
 
@@ -30,6 +26,8 @@ class CharmNatsK8SCharm(CharmBase):
 
     def __init__(self, *args):
         super().__init__(*args)
+        self._template_env = None
+
         self.framework.observe(self.on.nats_pebble_ready, self._on_nats_pebble_ready)
 
         self.framework.observe(self.on.config_changed,
@@ -41,26 +39,22 @@ class CharmNatsK8SCharm(CharmBase):
         self.framework.observe(self.on.cluster_relation_changed,
                                self._on_config_changed)
 
-        self.__START_NATS_CMD = f'nats-server -D -p={self.config["client_port"]} -m={self.config["monitor_port"]} ' \
-                                f'--cluster=nats://0.0.0.0:{self.config["cluster_port"]}'
-
         ingress_config = {"service-hostname": self.config["external_hostname"], "service-name": self.app.name,
-                                              "service-port": self.config["client_port"]}
+                          "service-port": self.config["client_port"]}
 
         logger.debug("Setting up ingress with env: {}".format(ingress_config))
 
         self.ingress = IngressRequires(self, ingress_config)
-
-        self._stored.set_default(things=[])
 
     def _on_nats_pebble_ready(self, event):
 
         container = event.workload
 
         cluster_relation = self.model.get_relation('cluster')
-        my_address = self._get_my_address(cluster_relation)
         all_unit_addresses = self._get_all_unit_addresses(
             cluster_relation)
+
+        self.__update_nats_config(container, all_unit_addresses)
 
         layer = self.nats_layer()
         services = container.get_plan().to_dict().get("services", {})
@@ -69,10 +63,6 @@ class CharmNatsK8SCharm(CharmBase):
             logging.info("Trying to add layer")
             container.add_layer("nats", layer, combine=True)
             logging.info("Added updated layer 'nats' to Pebble plan")
-
-        is_running = container.get_service('nats').is_running()
-        if is_running:
-            container.stop("nats")
 
         # Autostart any services that were defined with startup: enabled
         container.autostart()
@@ -86,39 +76,18 @@ class CharmNatsK8SCharm(CharmBase):
         """Adapt Nats config to Juju changes"""
         logging.debug('Handling Juju config change')
 
-        # layer = self.nats_layer()
-
         cluster_relation = self.model.get_relation('cluster')
-
+        # try?
         my_address = self._get_my_address(cluster_relation)
-        print(" ADDR =")
-        print(my_address)
         self._share_address_with_peers(my_address, cluster_relation)
 
         all_unit_addresses = self._get_all_unit_addresses(
             cluster_relation)
-        print("all ADDR =")
-        print(all_unit_addresses)
 
-        self.container = self.unit.get_container('nats')
+        container = self.unit.get_container('nats')
+        self.__update_nats_config(container, all_unit_addresses)
 
-        try:
-            is_running = self.container.get_service('nats').is_running()
-            if is_running:
-                self.container.stop("nats")
-        except ModelError as exc:
-            pass
-
-        #if self.container.get_service("nats").is_running():
-        #    self.container.stop("nats")
-
-        #try:
-        #    self.run_cmd('nats-server --signal stop', "stop")
-        #except ChangeError:
-        #    logging.info("NATS server already stopped")
-
-        routes_with_ports = [s + ":" + str(self.config["cluster_port"]) for s in all_unit_addresses]
-        self.run_cmd(f'{self.__START_NATS_CMD} --routes={",".join(routes_with_ports)}', "nats")
+        self.__restart_nats(container)
 
         logging.info("Restarted NATS service")
 
@@ -132,7 +101,7 @@ class CharmNatsK8SCharm(CharmBase):
                 self.__PEBBLE_SERVICE_NAME: {
                     "override": "replace",
                     "summary": "Nats server",
-                    "command": self.__START_NATS_CMD,
+                    "command": "nats-server --config /etc/nats/nats-server.conf",
                     "startup": "enabled",
                     "environment": {"KUBERNETES_POD_NAMESPACE": self.model.name,
                                     "NATS_SERVER": self.config["nats_version"]},
@@ -146,20 +115,21 @@ class CharmNatsK8SCharm(CharmBase):
         :param relation: the peer relation. See
                          https://github.com/canonical/operator/issues/534
         :type relation: ops.model.Relation
-        :returns: This unit's (first) ingress address.
+        :returns: This unit's  address.
         :rtype: str
         """
         network = self.model.get_binding(relation).network
         return str(network.bind_address)
 
     def _get_all_unit_addresses(self, relation):
-        """Get all ingress addresses shared by all peers over the relation.
+        """Get all addresses shared by all peers over the relation.
 
         Including the current unit.
 
         :param relation: the peer relation
         :type relation: ops.model.Relation
-        :returns: Each unit's (first) ingress address.
+        :returns: Each unit's
+         address.
         :rtype: List[str]
         """
         result = set()
@@ -179,44 +149,77 @@ class CharmNatsK8SCharm(CharmBase):
             if unit_address is not None:
                 result.add(unit_address)
 
-        logging.debug('All unit ingress addresses: {}'.format(
+        logging.debug('All unit addresses: {}'.format(
             ', '.join(result)))
 
         return list(result)
 
-    def _share_address_with_peers(self, my_ingress_address, relation):
-        """Share this unit's ingress address with peer units.
+    def __update_nats_config(self, workload_container, all_unit_addresses):
+        """Write NATS conf file to disk.
 
+        :param workload_container: the container in which NATS is running
+        :type workload_container: ops.model.Container
+        :param all_unit_addresses: Each unit's address.
+        :type all_unit_addresses: List[str]
+        """
+
+        config_file_path = '/etc/nats/nats-server.conf'
+
+        cluster_port = self.config['cluster_port']
+        client_port = self.config['client_port']
+        monitor_port = self.config['monitor_port']
+        user = self.config['user']
+        sys_user = self.config['system_user']
+        password = self.config['password']
+        sys_password = self.config['system_password']
+
+        routes_with_ports = [s + ":" + str(cluster_port) for s in all_unit_addresses]
+        routes_with_proto = ["nats://" + r for r in routes_with_ports]
+
+        routes_prepared = ",".join(routes_with_proto)
+
+        logging.debug('Writing config to {}'.format(config_file_path))
+
+        self._push_template(workload_container, 'nats-server.conf', config_file_path,
+                            {'cluster_port': cluster_port, 'client_port': client_port,
+                             'monitor_port': monitor_port, 'routes': routes_prepared,
+                             'user': user, 'password': password, "sys_user": sys_user,
+                             "sys_pass": sys_password})
+
+    def __restart_nats(self, workload_container):
+        """Restart NATS by restarting the Pebble services.
+
+        :param workload_container: the container in which NATS is running
+        :type workload_container: ops.model.Container
+        """
+        services = workload_container.get_plan().to_dict().get('services', {})
+        if not len(services):
+            # No Pebble service defined yet, too early:
+            return
+
+        logging.info('Restarting NATS...')
+        workload_container.stop(self.__PEBBLE_SERVICE_NAME)
+        # Autostart any services that were defined with startup: enabled :
+        workload_container.autostart()
+
+    def _push_template(self, container, template_name, target_path, context={}):
+        if self._template_env is None:
+            self._template_env = Environment(loader=FileSystemLoader(
+                f'{self.charm_dir}/templates'))
+        container.push(
+            target_path,
+            self._template_env.get_template(template_name).render(**context),
+            make_dirs=True
+        )
+
+    def _share_address_with_peers(self, my_address, relation):
+        """Share this unit's address with peer units.
         :param relation: the peer relation
         :type relation: ops.model.Relation
         """
         relation.data[self.unit][self.__ADDR_CLUSTER_REL_DATA_KEY] = (
-            my_ingress_address)
+            my_address)
 
-    def run_cmd(self, cmd, label="cmd", env=None):
-        layer = {
-            "services": {
-                label: {
-                    "override": "replace",
-                    "startup": "disabled",
-                    "command": cmd,
-                    "environment": env or {},
-                }
-            }
-        }
-        logger.info("running cmd: %s", cmd)
-        self.container.add_layer(label, layer, combine=True)
-
-        try:
-            self.container.start(label)
-        except ChangeError as exc:
-            #  Start service "cmd" (cannot start service: exited quickly with code 0)
-            if "exited quickly with code 0" in exc.err:
-                logger.info("cmd succeed")
-                return True
-            else:
-                logger.exception("cmd failed")
-                return False
 
 if __name__ == "__main__":
     main(CharmNatsK8SCharm)
